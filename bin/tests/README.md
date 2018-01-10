c3e29eb76c723604def6959610dac38be6ade050
Sebastien Duthil <sduthil@wazo.community>
2018-01-10 15:27:35 -0500

    add some minimal tests

---
+++ bin/tests/README.md
@@ -0,0 +1,5 @@
+# xivo-certs tests
+
+This minimal test must be run from the project root in order to work:
+
+  nosetests bin/tests
---
+++ bin/tests/test_certificate_needs_subjectaltname.py
@@ -0,0 +1,18 @@
+# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+import subprocess
+
+from hamcrest import (
+    assert_that,
+    equal_to,
+)
+
+
+def test_certificate_needs_subjectaltname_true():
+    return_code = subprocess.call(['bin/certificate-needs-subjectaltname', 'integration_tests/assets/certificate-without-subjectaltname.crt'])
+    assert_that(return_code, equal_to(0))
+    return_code = subprocess.call(['bin/certificate-needs-subjectaltname', 'integration_tests/assets/certificate-not-generated-by-wazo.crt'])
+    assert_that(return_code, equal_to(1))
+    return_code = subprocess.call(['bin/certificate-needs-subjectaltname', 'integration_tests/assets/certificate-with-subjectaltname.crt'])
+    assert_that(return_code, equal_to(1))
